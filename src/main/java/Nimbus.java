f163e44432512d9a2b78557335b98d1a887a26d3
Adrian Luah Jun Kang <adrian.luahjk@gmail.com>
2026-08-20 15:45:05 +0800

    Level-0 Chatbot renaming

---
+++ src/main/java/Nimbus.java
@@ -0,0 +1,20 @@
+public class Nimbus {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String banner =
+                  "#   #  #####  #   #  ####   #   #   ####\n"
+                + "##  #    #    ## ##  #   #  #   #  #     \n"
+                + "# # #    #    # # #  ####   #   #   ###  \n"
+                + "#  ##    #    #   #  #   #  #   #     #  \n"
+                + "#   #  #####  #   #  ####    ###   ####  \n";
+
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Nimbus.");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
