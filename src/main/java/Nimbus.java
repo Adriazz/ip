9bdd3e881c57507e8116d1d5ff578c5bda9c6d06
Adrian Luah Jun Kang <adrian.luahjk@gmail.com>
2026-08-20 22:40:33 +0800

    Level-1 Echo

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Nimbus {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String horizontalLine = "____________________________________________________________";
         String banner =
                   "#   #  #####  #   #  ####   #   #   ####\n"
@@ -13,8 +15,17 @@ public class Nimbus {
         System.out.println("Hello! I'm Nimbus.");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        
+
+        String input = scanner.nextLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+            input = scanner.nextLine();
+        }
+        System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
+        scanner.close();
     }
 }
